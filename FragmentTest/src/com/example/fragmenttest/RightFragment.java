90778cbf14916217ab5e8f37f9aa8da3e160ae68
harvey-ren <1448616686@qq.com>
2014-07-01 17:09:40 +0800

    Fragment的使用方法
    Fragment的使用

---
+++ FragmentTest/src/com/example/fragmenttest/RightFragment.java
@@ -0,0 +1,19 @@
+package com.example.fragmenttest;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+@SuppressLint("NewApi")
+public class RightFragment extends Fragment
+{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View view = inflater.inflate(R.layout.right, null);
+		return view;
+	}
+}
