ab7578111eea490443ba2262990335d281e94e7f
harvey-ren <1448616686@qq.com>
2014-07-16 10:10:51 +0800

    完善代码
    完善代码

--- NetModule/src/com/iwise/net/RequestUrl.java
+++ NetModule/src/com/iwise/net/RequestUrl.java
@@ -14,10 +14,10 @@ public class RequestUrl
 	/**
 	 * 主地址
 	 */
-	public static final String MAIN_REQUEST_URL = "http://i.funtalk.cn/cloudservice/";
+	public static final String MAIN_REQUEST_URL = "";
 
 	/**
 	 * 初始化接口
 	 */
-	public static final String INITINFO = MAIN_REQUEST_URL + "initinfo.action";
+	public static final String INITINFO = MAIN_REQUEST_URL + "";
 }
