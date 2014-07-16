a4ea8c93000cc0ad8ce06123c7669c4f86eb65ef
harvey-ren <1448616686@qq.com>
2014-07-16 14:47:35 +0800

    添加注释
    添加注释

--- ServiceTest/src/com/iwise/test/MainActivity.java
+++ ServiceTest/src/com/iwise/test/MainActivity.java
@@ -1,7 +1,12 @@
 package com.iwise.test;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 
 /**
@@ -14,34 +19,56 @@ import android.view.View;
  * 
  */
 
-public class MainActivity extends Activity
+public class MainActivity extends Activity implements ServiceConnection
 {
+
+	private Intent serviceIntent = null;
+	private RZService service = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		serviceIntent = new Intent(this, RZService.class);
 	}
 
 	public void startService(View view)
 	{
 		System.out.println("startService");
+		startService(serviceIntent);
 	}
 
 	public void stopService(View view)
 	{
 		System.out.println("stopService");
+		stopService(serviceIntent);
 	}
 
 	public void bindService(View view)
 	{
 		System.out.println("bindService");
+		bindService(serviceIntent, this, Context.BIND_AUTO_CREATE);
 	}
 
 	public void unbindService(View view)
 	{
 		System.out.println("unbindService");
+		unbindService(this);
+		service = null;
+	}
+
+	@Override
+	public void onServiceConnected(ComponentName name, IBinder binder)
+	{
+		System.out.println("onServiceConnected");
+		service = ((RZService.RZServiceBinder) binder).getService();
+	}
+
+	@Override
+	public void onServiceDisconnected(ComponentName name)
+	{
+
 	}
 
 }
---
+++ ServiceTest/src/com/iwise/test/RZService.java
@@ -0,0 +1,109 @@
+package com.iwise.test;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+/**
+ * 自写Service
+ * 
+ * @File: MyService.java
+ * @Package com.iwise.test
+ * @Description:
+ * @author Harvey
+ * @date 2014-7-16 下午1:48:05
+ * 
+ */
+public class RZService extends Service
+{
+
+	private Timer timer = null;
+	private TimerTask task = null;
+	private int i = 0;
+
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+		System.out.println("onCreate()");
+		startTimer();
+	}
+
+	/**
+	 * 开始Timer
+	 * 
+	 * @Title: startTimer
+	 * @Description:
+	 * @param
+	 * @return void 返回类型
+	 * @throws
+	 */
+	private void startTimer()
+	{
+		if (timer == null)
+		{
+			timer = new Timer();
+			task = new TimerTask()
+			{
+				@Override
+				public void run()
+				{
+					i++;
+					System.out.println(i);
+				}
+			};
+			timer.schedule(task, 1000, 1000);
+		}
+	}
+
+	public class RZServiceBinder extends Binder
+	{
+		public RZService getService()
+		{
+			return RZService.this;
+		}
+	}
+
+	private final RZServiceBinder serviceBinder = new RZServiceBinder();
+
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		System.out.println("onBind");
+		return serviceBinder;
+	}
+
+	@Override
+	public void onDestroy()
+	{
+		super.onDestroy();
+		System.out.println("onDestroy()");
+		stopTimer();
+	}
+
+	/**
+	 * 停止Timer
+	 * 
+	 * @Title: stopTimer
+	 * @Description:
+	 * @param 设定文件
+	 * @return void 返回类型
+	 * @throws
+	 */
+	private void stopTimer()
+	{
+		if (timer != null)
+		{
+			timer.cancel();
+			task.cancel();
+
+			task = null;
+			timer = null;
+		}
+	}
+
+}
