e4780ce975bdcf20b2b86307fa5514dd692c47c3
harvey-ren <1448616686@qq.com>
2014-07-16 10:06:39 +0800

    新建图片缓存模块
    新建图片缓存模块

---
+++ CacheModule/src/com/iwise/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.iwise.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
